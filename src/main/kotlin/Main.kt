3c390099e7ae164e43bbb74276a25b1744248157
Pfaffenrodt <dimitri@pfaffenrodt.de>
2021-11-19 19:30:10 +0100

    :recycle: remove java scanner and use kotline readline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,26 @@
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import java.util.*
 
 suspend fun main() = coroutineScope  {
     launch {
         intro()
-    }
-    val scanner = Scanner(System.`in`)
 
-    while (scanner.hasNext()) {
-        val next = scanner.next()
-
-        println("Picked mode: $next")
-
-        when (next) {
+        do {
+            val next = readLine()
+            if (next != null) {
+                println("Picked mode: $next")
+            }
+            when(next) {
 //            "1" -> simpleGame()
 //            "2" -> againstARock()
 //            "3" -> simulateGames()
-            "exit" -> break
-            else -> {
-                println("invalid argument: $next")
-                printHelp()
+                null, "exit" -> return@launch
+                else -> {
+                    println("invalid argument: $next")
+                    printHelp()
+                }
             }
-        }
+        } while (true)
     }
-    scanner.close()
+    println()
 }
